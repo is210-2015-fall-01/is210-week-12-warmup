41880d44d46d9688ac584af8305f91755de22227
Christopher Fernanders <cj.fernanders1986@gmail.com>
2015-11-10 15:09:25 -0500

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 01 module"""
+"""Task 01"""
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """No key access.
+    Attributes:
+        var1(mixed): an argument variable
+        var2(mixed): an argument variable
+    Returns:
+        Will return an exception.
+    Example:
+        >>> simple_lookup([1,2], 4)
+        Warning: Your index/key doesn't exist.
+        [1,2]
+        >>> simple_lookup({}, 'banana')
+        Warning: Your index/key doesn't exist.
+        {}
+    """
+    try:
+        return var1[var2]
+    except LookupError:
+        print 'Warning: Your index/key does not exist.'
+        return var1
