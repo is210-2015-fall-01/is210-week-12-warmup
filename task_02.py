78aefa700d27a3b83c854e4ae4a5463b3523d111
Christopher Fernanders <cj.fernanders1986@gmail.com>
2015-11-10 15:11:48 -0500

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 02 module"""
+"""Task 02"""
 
 
 import datetime
 
 
 class InvalidAgeError(Exception):
+    """To obtain a valid age."""
     pass
 
+
 def get_age(birthyear):
+    """An age exception.
+    Args:
+        birthyear(int): age of user
+    Returns:
+        Will return an invalid age error when age is not valid.
+    Example:
+        >>> get_age(2099)
+        Traceback (most recent call last):
+        File "<stdin>", line 1, in <module>
+        __main__.InvalidAgeError
+    """
     age = datetime.datetime.now().year - birthyear
-    return age
+    if age >= 0:
+        return age
+    else:
+        raise InvalidAgeError
