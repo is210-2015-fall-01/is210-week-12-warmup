956b2bb09f7d64a039786928861fb6d1630c6311
Shinseeker <dailymanj88@live.com>
2015-11-11 16:16:05 +0000

    revision on task 3

--- task_03.py
+++ task_03.py
@@ -10,38 +10,32 @@ class CustomLogger(object):
     """CustomLogger class"""
 
     def __init__(self, logfilename):
-        """A function"""
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
-        """A log function."""
+        """A log function"""
         if timestamp is None:
             timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
-        """A flush function."""
-        handled = []
-
-        for index, entry in enumerate(self.msgs):
-            try:
-                fhandler = open(self.logfilename, 'a')
-            except IOError:
-                self.log(msg='IOError')
-                raise IOError
-            else:
-                self.log(msg='other Error')
-                break
-            finally:
-                fhandler.close()
-            try:
+        """A flush function"""
+        try:
+            handled = []
+            fhandler = open(self.logfilename, 'a')
+        except IOError:
+            raise IOError
+        try:
+            for index, entry in enumerate(self.msgs):
                 fhandler.write(str(entry) + '\n')
                 handled.append(index)
-                for index in handled[::-1]:
-                    del self.msgs[index]
-            except IOError:
-                self.log(msg='IOError')
-                break
-            finally:
-                fhandler.close()
+        except IOError:
+            raise IOError
+        try:
+            for index in handled[::-1]:
+                del self.msgs[index]
+        except IOError:
+            raise IOError
+        finally:
+            fhandler.close()
