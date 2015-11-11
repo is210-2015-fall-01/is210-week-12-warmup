4c545d5fed71016448b240f6477cfc16f24c9366
Vagrant User <dailymanj88@live.com>
2015-11-11 15:29:54 +0000

    w12 warmup tasks done

--- task_03.py
+++ task_03.py
@@ -7,25 +7,41 @@ import time
 
 
 class CustomLogger(object):
+    """CustomLogger class"""
 
     def __init__(self, logfilename):
+        """A function"""
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
+        """A log function."""
         if timestamp is None:
             timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
+        """A flush function."""
         handled = []
 
-        fhandler = open(self.logfilename, 'a')
         for index, entry in enumerate(self.msgs):
-            fhandler.write(str(entry) + '\n')
-            handled.append(index)
-
-        fhandler.close()
-
-        for index in handled[::-1]:
-            del self.msgs[index]
+            try:
+                fhandler = open(self.logfilename, 'a')
+            except IOError:
+                self.log(msg='IOError')
+                raise IOError
+            else:
+                self.log(msg='other Error')
+                break
+            finally:
+                fhandler.close()
+            try:
+                fhandler.write(str(entry) + '\n')
+                handled.append(index)
+                for index in handled[::-1]:
+                    del self.msgs[index]
+            except IOError:
+                self.log(msg='IOError')
+                break
+            finally:
+                fhandler.close()
