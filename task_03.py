ddd7eca3bac499acaf41a807ab81c86e76778e4c
Nick Ileczko <nickileczko@gmail.com>
2015-11-12 19:52:17 +0000

    task3

--- task_03.py
+++ task_03.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 03 module"""
+"""Week 12 Warmup Task 03."""
 
 
 import time
 
 
 class CustomLogger(object):
+    """CustomerLogger object."""
 
     def __init__(self, logfilename):
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
+        """Log function."""
         if timestamp is None:
             timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
+        """Flush function."""
         handled = []
+        try:
+            fhandler = open(self.logfilename, 'a')
+        except IOError:
+            self.log('Cannot Open Log File')
+            raise IOError
 
-        fhandler = open(self.logfilename, 'a')
         for index, entry in enumerate(self.msgs):
-            fhandler.write(str(entry) + '\n')
-            handled.append(index)
-
-        fhandler.close()
+            try:
+                fhandler.write(str(entry) + '\n')
+                handled.append(index)
+            except IOError:
+                self.log('Cannot Write Log File')
 
         for index in handled[::-1]:
             del self.msgs[index]
+        fhandler.close()
