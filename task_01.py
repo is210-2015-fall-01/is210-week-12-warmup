fe5a5b9197d53f81ada5d0a020792a7fa231a24f
Nick Ileczko <nickileczko@gmail.com>
2015-11-12 19:52:00 +0000

    task1

--- task_01.py
+++ task_01.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 01 module"""
+"""Week 12 Warmup Task 01."""
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """Index Search.
+    Attributes:
+        var1(mixed): variable.
+        var2(mixed): variable.
+    Return:
+        var1
+    Example:
+    >>> simple_lookup([1,2], 4)
+    Warning: Your index/key doesn't exist.
+    [1,2]
+    >>> simple_lookup({}, 'banana')
+    Warning: Your index/key doesn't exist.
+    {}
+    """
+    try:
+        return var1[var2]
+    except LookupError:
+        print 'Warning: Your index/key doesn\'t exist.'
+        return var1
