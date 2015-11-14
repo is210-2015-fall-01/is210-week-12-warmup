14fc30dbdb1890474f993907a11b4721edfbb502
emad8878 <emad5223@gmail.com>
2015-11-14 09:41:53 +0000

    Week 12 warmupp task

--- task_01.py
+++ task_01.py
@@ -4,4 +4,28 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """This function attempts to access any index or key.
+
+    Args:
+        var1(mix): any value.
+        var2(mix): any value.
+
+    Return:
+        Return index or key.
+
+    Examples:
+        >>> simple_lookup([1, 2], 4)
+        Warning: Your index/key doesn't exist.
+        [1, 2]
+        >>> simple_lookup({}, 'banana')
+        Warning: Your index/key doesn't exist.
+         }
+    """
+    try:
+        return var1[var2]
+    except IndexError:
+        print 'Warning: Your index/key doesn\'t exist.'
+        return var1
+    except KeyError:
+        print 'Warning: Your index/key doesn\'t exist.'
+        return var1
--- task_02.py
+++ task_02.py
@@ -7,8 +7,28 @@ import datetime
 
 
 class InvalidAgeError(Exception):
+    """This class inform invaild error."""
+
     pass
 
+
 def get_age(birthyear):
+    """This funcation calls and return valid birth year.
+
+    Args:
+        birthyears(int): birth year.
+
+    Returns:
+        age(int): birth year.
+
+    Examples:
+        >>> get_age(2099)
+        Traceback (most recent call last):
+        File "<stdin>", line 1, in <module>
+        __main__.InvalidAgeError
+    """
     age = datetime.datetime.now().year - birthyear
-    return age
+    if age < 0:
+        raise InvalidAgeError
+    else:
+        return age
