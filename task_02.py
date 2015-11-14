fcef55882317942c8ea0a7fb76654de324238cd1
Shavon Brown <slb6968@hotmail.com>
2015-11-14 06:06:52 +0000

    Completed 3 tasks and successfully executed tests git statusgit status

--- task_02.py
+++ task_02.py
@@ -7,8 +7,24 @@ import datetime
 
 
 class InvalidAgeError(Exception):
+    """Class docstring"""
     pass
 
+
 def get_age(birthyear):
+    """Converts birth year to age.
+
+    Args:
+        birthyear (numeric): should be a 4-digit number
+
+    Returns:
+        age (numeric): equals current year minus birthyear
+
+    Raises:
+        InvalidAgeError: If age is less than zero.
+    """
     age = datetime.datetime.now().year - birthyear
-    return age
+    if age < 0:
+        raise InvalidAgeError
+    else:
+        return age
