6f62a6b761f2203fd5bdec6f51111c746b38ffd9
Farzad Hejazi <fhejazi@gmail.com>
2015-11-14 04:42:15 +0000

    Week 12 Warm-Up Assignments.

--- task_01.py
+++ task_01.py
@@ -4,4 +4,21 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """This is a simple lookup module to demonstrate exception handling.
+
+    Args:
+        var1 (list): A list.
+        var2 (int): An index of the list.
+
+    Returns:
+        var1[var2]: The element in index var2 of var1 list.
+
+    Example:
+        >>> simple_lookup([1,2], 1)
+        2
+"""
+    try:
+        return var1[var2]
+    except LookupError:
+        print 'Warning: Your index/key doesn\'t exist.'
+        return var1
--- task_03.py
+++ task_03.py
@@ -7,25 +7,54 @@ import time
 
 
 class CustomLogger(object):
+    """This is a CustomLogger class"""
 
     def __init__(self, logfilename):
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
+        """A log function.
+
+        This function appends a timestamp and message string
+        to the msg list.
+
+        Args:
+            msg (str): A message string.
+            timestamp (time, default=None): The time stamp.
+        Returns:
+            None
+        """
         if timestamp is None:
             timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
+        """A flush function.
+
+        This function writes the contents of the msg list into
+        a log file.
+
+        Args:
+            None
+        Returns:
+            None
+        """
         handled = []
 
-        fhandler = open(self.logfilename, 'a')
-        for index, entry in enumerate(self.msgs):
-            fhandler.write(str(entry) + '\n')
-            handled.append(index)
-
-        fhandler.close()
-
-        for index in handled[::-1]:
-            del self.msgs[index]
+        try:
+            fhandler = open(self.logfilename, 'a')
+        except IOError:
+            self.log('Log file cannot be opened.')
+            raise IOError()
+        else:
+            for index, entry in enumerate(self.msgs):
+                try:
+                    fhandler.write(str(entry) + '\n')
+                    handled.append(index)
+                except IOError:
+                    self.log('Cannot write to file.')
+                    continue
+            for index in handled[::-1]:
+                del self.msgs[index]
+            fhandler.close()
