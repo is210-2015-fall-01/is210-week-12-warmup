f22f4a4bf7babf4fdb32602f646d65a36cd4cc32
Shavon Brown <slb6968@hotmail.com>
2015-11-14 01:12:31 -0500

    Update task_01.py
    Changed double quotes to single quotes. (line 22)

--- task_01.py
+++ task_01.py
@@ -19,5 +19,5 @@ def simple_lookup(var1, var2):
     try:
         return var1[var2]
     except LookupError:
-        print "Warning: Your index/key doesn't exist."
+        print 'Warning: Your index/key doesn\'t exist.'
         return var1
